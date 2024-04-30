d97272c52ca0a378bb3419c01d42941d3da74524
Vikesh Bansal <vikesh.bansal1@gmail.com>
2024-04-30 13:27:30 +0530

    specified connection string of DB

--- Wpm.Web/Program.cs
+++ Wpm.Web/Program.cs
@@ -4,7 +4,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
-builder.Services.AddInMemoryWpmDb();
+builder.Services.AddWpmDb(builder.Configuration);
 
 var app = builder.Build();
 
